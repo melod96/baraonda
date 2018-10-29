e6d83be577d363134d2c5091be3dd240ca1afa32
user2 <user2@KH_A>
2018-10-29 21:04:19 +0900

    소진 업데이트 181029 메인 공지사항 select 하는중

---
+++ src/main/java/com/kh/baraonda/main/model/service/MainService.java
@@ -0,0 +1,21 @@
+package com.kh.baraonda.main.model.service;
+
+import java.util.ArrayList;
+
+import org.apache.tools.ant.Main;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.baraonda.main.model.dao.MainDao;
+import com.kh.baraonda.main.model.exception.NoticeSelectListException;
+import com.kh.baraonda.main.model.vo.PageInfo;
+import com.kh.baraonda.notice.model.vo.Notice;
+
+@Service
+public interface MainService {
+	
+	ArrayList<Notice> selectNotice() throws NoticeSelectListException;
+	
+
+}
