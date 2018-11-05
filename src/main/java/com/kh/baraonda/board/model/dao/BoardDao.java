18257aef5ca6b7dfc3cd9e6b5234f0d75d14cfed
user2 <user2@KH_A>
2018-11-05 21:08:21 +0900

    댓글 작성하는중

--- src/main/java/com/kh/baraonda/board/model/dao/BoardDao.java
+++ src/main/java/com/kh/baraonda/board/model/dao/BoardDao.java
@@ -7,12 +7,15 @@ import org.mybatis.spring.SqlSessionTemplate;
 
 import com.kh.baraonda.board.model.exception.BoardException;
 import com.kh.baraonda.board.model.vo.Board;
+import com.kh.baraonda.board.model.vo.Comments;
+import com.kh.baraonda.common.PageInfo;
 import com.kh.baraonda.member.model.vo.Member;
+import com.kh.baraonda.myPage.model.vo.Files;
 
 public interface BoardDao {
 	
 	//게시글 전체 목록 조회
-	public List<HashMap<String, Object>> listAll(SqlSessionTemplate sqlSession, int writing_type) throws BoardException;
+	public List<HashMap<String, Object>> listAll(SqlSessionTemplate sqlSession, int writing_type, PageInfo info) throws BoardException;
 	//게시글 페이징 처리
 	public int selectBoardListCount(SqlSessionTemplate sqlSession);
 	
@@ -26,7 +29,12 @@ public interface BoardDao {
 	/*public List<HashMap<String, Object>> detail(SqlSessionTemplate sqlSession, int board_no) throws BoardException;*/
 	public HashMap<String, Object> detail(SqlSessionTemplate sqlSession, int board_no) throws BoardException;
 	
+	//댓글 작성
+	public void comment(Comments c) throws BoardException;
 	//댓글 조회
 	public List<HashMap<String, Object>> commentList(SqlSessionTemplate sqlSession, int board_no) throws BoardException;
+	
+	//게시글 작성시 사진 업로드
+	public void insertPhoto(SqlSessionTemplate sqlSession, Files file) throws BoardException;
 	
 }
